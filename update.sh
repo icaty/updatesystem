7a3e4150209c806ff94a89ae1f9c0bf23c62e6c6
icaty <115490678+icaty@users.noreply.github.com>
2022-10-10 22:26:40 +0530

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+figlet Update Your SyS | lolcat
+sleep 2
+clear
+sleep 1
+echo "Hello.. Your System Is Updating in 4 Sec"
+sleep 4
+clear
+sudo apt-get update
+sleep 2
+sudo apt-get upgrade
+sleep 1
+echo "Thanks For Using..."
+
